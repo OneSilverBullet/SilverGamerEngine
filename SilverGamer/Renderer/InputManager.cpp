b9325208feb3d9264a226dd9e7c795801c740bfb
SilverGamer <1795959147@qq.com>
2021-10-29 00:46:44 +0800

    #15 完成callback函数的绑定和控制管理器。

--- SilverGamer/Renderer/InputManager.cpp
+++ SilverGamer/Renderer/InputManager.cpp
@@ -1,5 +1,6 @@
 #include "InputManager.h"
 
+int Renderer::SGInputManager::m_cursorStatus = 1;
 KEY_STATUS Renderer::SGInputManager::m_keyStatus = { false };
 MOUSE_BUTTON_STATUS Renderer::SGInputManager::m_mouseButtonStatus = { false };
 CURSOR_STATUS Renderer::SGInputManager::m_cursorPos = { 0.0 };
@@ -12,68 +13,169 @@ std::vector<std::function<void(double, double)>> m_cursorCallBackFunctions;
 std::vector<std::function<void(double, double)>> m_scrollCallBackFunctions;
 std::vector<std::function<void(int, int, int, int)>> m_keyCallbackFunctions;
 
-
-
-void Renderer::SGInputManager::Init()
+void Renderer::SGInputManager::Init(GLFWwindow* window)
 {
+	RegistCallbackFunc(window);
 }
 
 int Renderer::SGInputManager::RegistKeyCallbackFunc(std::function<void(int, int, int, int)> vkeyCallback)
 {
-	return 0;
+	m_keyCallbackFunctions.push_back(vkeyCallback);
+	return static_cast<int>(m_keyCallbackFunctions.size()) - 1; //返回当前call back函数的索引
 }
 
 int Renderer::SGInputManager::RegistCursorCallback(std::function<void(double, double)> vcursorCallback)
 {
-	return 0;
+	m_cursorCallBackFunctions.push_back(vcursorCallback);
+	return static_cast<int>(m_cursorCallBackFunctions.size()) - 1; //返回当前call back函数的索引
 }
 
 int Renderer::SGInputManager::RegistMouseButtonCallback(std::function<void(int, int, int)> vmouseCallback)
 {
-	return 0;
+	m_mouseButtonCallBackFuntions.push_back(vmouseCallback);
+	return static_cast<int>(m_mouseButtonCallBackFuntions.size()) - 1; //返回当前call back函数的索引
 }
 
 int Renderer::SGInputManager::RegistScrollCallback(std::function<void(double, double)> vscrollCallback)
 {
-	return 0;
+	m_scrollCallBackFunctions.push_back(vscrollCallback);
+	return static_cast<int>(m_scrollCallBackFunctions.size()) - 1; //返回当前call back函数的索引
 }
 
-int Renderer::SGInputManager::UnregistKeyCallbackFunc(std::function<void(int, int, int, int)> vkeyCallback)
+void Renderer::SGInputManager::UnregistKeyCallbackFunc(std::function<void(int, int, int, int)> vkeyCallback)
 {
-	return 0;
+	
 }
 
-int Renderer::SGInputManager::UnregistCursorCallback(std::function<void(double, double)> vcursorCallback)
+void Renderer::SGInputManager::UnregistCursorCallback(std::function<void(double, double)> vcursorCallback)
 {
-	return 0;
+
 }
 
-int Renderer::SGInputManager::UnregistMouseButtonCallback(std::function<void(int, int, int)> vmouseCallback)
+void Renderer::SGInputManager::UnregistMouseButtonCallback(std::function<void(int, int, int)> vmouseCallback)
 {
-	return 0;
+
 }
 
-int Renderer::SGInputManager::UnregistScrollCallback(std::function<void(double, double)> vscrollCallback)
+void Renderer::SGInputManager::UnregistScrollCallback(std::function<void(double, double)> vscrollCallback)
 {
-	return 0;
+
 }
 
-int Renderer::SGInputManager::UnregistKeyCallbackFunc(int vEraseIndex)
+void Renderer::SGInputManager::UnregistKeyCallbackFunc(int vEraseIndex)
 {
-	return 0;
+	m_keyCallbackFunctions.erase(m_keyCallbackFunctions.begin() + vEraseIndex);
 }
 
-int Renderer::SGInputManager::UnregistCursorCallback(int vEraseIndex)
+void Renderer::SGInputManager::UnregistCursorCallback(int vEraseIndex)
 {
-	return 0;
+	m_cursorCallBackFunctions.erase(m_cursorCallBackFunctions.begin() + vEraseIndex);
 }
 
-int Renderer::SGInputManager::UnregistMouseButtonCallback(int vEraseIndex)
+void Renderer::SGInputManager::UnregistMouseButtonCallback(int vEraseIndex)
 {
-	return 0;
+	m_mouseButtonCallBackFuntions.erase(m_mouseButtonCallBackFuntions.begin() + vEraseIndex);
 }
 
-int Renderer::SGInputManager::UnregistScrollCallback(int vEraseIndex)
+void Renderer::SGInputManager::UnregistScrollCallback(int vEraseIndex)
 {
-	return 0;
+	m_scrollCallBackFunctions.erase(m_scrollCallBackFunctions.begin() + vEraseIndex);
+}
+
+const KEY_STATUS& Renderer::SGInputManager::GetKeyStatus() const
+{
+	return m_keyStatus;
+}
+
+const MOUSE_BUTTON_STATUS& Renderer::SGInputManager::GetMouseButtonStatus() const
+{
+	return m_mouseButtonStatus;
+}
+
+const CURSOR_STATUS& Renderer::SGInputManager::GetCursorPos() const
+{
+	return m_cursorPos;
+}
+
+const CURSOR_STATUS& Renderer::SGInputManager::GetCursorOffset() const
+{
+	return m_cursorOffset;
+}
+
+const SCROLL_OFFSET_STATUS& Renderer::SGInputManager::GetScrollJourney() const
+{
+	return m_scrollJourney;
+}
+
+Renderer::SGInputManager::SGInputManager()
+{
+}
+
+//According to window*, bind callback function
+void Renderer::SGInputManager::RegistCallbackFunc(GLFWwindow* window)
+{
+	glfwSetKeyCallback(window, KeyCallbackFunc);
+	glfwSetMouseButtonCallback(window, MouseButtonCallbackFunc);
+	glfwSetCursorPosCallback(window, CursorCallbackFunc);
+	glfwSetScrollCallback(window, ScrollCallbackFunc);
+}
+
+void Renderer::SGInputManager::KeyCallbackFunc(GLFWwindow* window, GLint vKey, GLint vScancode, GLint vAction, GLint vMode)
+{
+	//immediately check the close state
+	if (vKey == GLFW_KEY_ESCAPE && vAction == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	m_keyStatus[vKey] = vAction;
+
+	if (m_keyStatus[GLFW_KEY_LEFT_ALT] == GLFW_PRESS && m_keyStatus[GLFW_KEY_C] == GLFW_PRESS) {
+		m_cursorStatus = !m_cursorStatus;
+		glfwSetInputMode(window, GLFW_CURSOR, m_cursorStatus ? GLFW_CURSOR_NORMAL : GLFW_CURSOR_DISABLED);
+		//cursor initialize flag is 2 firstly
+		if (m_cursorStatus == 1)
+			m_cursorStatus = 2;
+	}
+
+	//run the binding functions
+	for (auto func : m_keyCallbackFunctions) {
+		func(vKey, vScancode, vAction, vMode);
+	}
+}
+
+void Renderer::SGInputManager::CursorCallbackFunc(GLFWwindow* window, GLdouble vPosX, GLdouble vPosY)
+{
+	//Log the cursor information
+	m_cursorPos[0] = vPosX;
+	m_cursorPos[1] = vPosY;
+	if (m_cursorStatus == 2)
+	{
+		m_cursorOffset[0] = m_cursorOffset[1] = 0;
+		m_cursorStatus = 1;
+	}
+	else
+	{
+		m_cursorOffset[0] = m_cursorPos[0] - m_lastCursorPos[0];
+		m_cursorOffset[1] = m_lastCursorPos[1] - m_cursorPos[1];
+	}
+	m_lastCursorPos = m_cursorPos;
+	for (auto func : m_cursorCallBackFunctions) {
+		func(vPosX, vPosY);
+	}
+}
+
+void Renderer::SGInputManager::ScrollCallbackFunc(GLFWwindow* window, GLdouble vOffsetX, GLdouble vOffsetY)
+{
+	m_scrollJourney[0] += vOffsetX;
+	m_scrollJourney[1] += vOffsetY;
+	for (auto func : m_scrollCallBackFunctions) {
+		func(vOffsetX, vOffsetY);
+	}
+}
+
+void Renderer::SGInputManager::MouseButtonCallbackFunc(GLFWwindow* window, GLint vButton, GLint vAction, GLint vModes)
+{
+	m_mouseButtonStatus[vButton] = vAction;
+	for (auto func : m_mouseButtonCallBackFuntions) {
+		func(vButton, vAction, vModes);
+	}
 }
--- SilverGamer/Renderer/InputManager.h
+++ SilverGamer/Renderer/InputManager.h
@@ -26,26 +26,31 @@ namespace Renderer
 	class SGInputManager :  public SGSingleton<SGInputManager>
 	{
 	public:
-		void	Init();
+		void	Init(GLFWwindow* window);
 		int RegistKeyCallbackFunc(std::function<void(int, int, int, int)> vkeyCallback);
 		int RegistCursorCallback(std::function<void(double, double)> vcursorCallback);
 		int RegistMouseButtonCallback(std::function<void(int, int, int)> vmouseCallback);
 		int RegistScrollCallback(std::function<void(double, double)> vscrollCallback);
 
-		int UnregistKeyCallbackFunc(std::function<void(int, int, int, int)> vkeyCallback);
-		int UnregistCursorCallback(std::function<void(double, double)> vcursorCallback);
-		int UnregistMouseButtonCallback(std::function<void(int, int, int)> vmouseCallback);
-		int UnregistScrollCallback(std::function<void(double, double)> vscrollCallback);
+		void UnregistKeyCallbackFunc(std::function<void(int, int, int, int)> vkeyCallback);
+		void UnregistCursorCallback(std::function<void(double, double)> vcursorCallback);
+		void UnregistMouseButtonCallback(std::function<void(int, int, int)> vmouseCallback);
+		void UnregistScrollCallback(std::function<void(double, double)> vscrollCallback);
 		
-		int UnregistKeyCallbackFunc(int vEraseIndex);
-		int UnregistCursorCallback(int vEraseIndex);
-		int UnregistMouseButtonCallback(int vEraseIndex);
-		int UnregistScrollCallback(int vEraseIndex);
+		void UnregistKeyCallbackFunc(int vEraseIndex);
+		void UnregistCursorCallback(int vEraseIndex);
+		void UnregistMouseButtonCallback(int vEraseIndex);
+		void UnregistScrollCallback(int vEraseIndex);
 
-
+		bool GetCursorStatus() { return m_cursorStatus; }
+		const KEY_STATUS& GetKeyStatus() const;
+		const MOUSE_BUTTON_STATUS& GetMouseButtonStatus() const;
+		const CURSOR_STATUS& GetCursorPos() const;
+		const CURSOR_STATUS& GetCursorOffset() const;
+		const SCROLL_OFFSET_STATUS& GetScrollJourney() const;
 
 	private:
-
+		static int m_cursorStatus;
 		static KEY_STATUS m_keyStatus;
 		static MOUSE_BUTTON_STATUS m_mouseButtonStatus;
 		static CURSOR_STATUS m_cursorPos;
@@ -58,8 +63,12 @@ namespace Renderer
 		static std::vector<std::function<void(double, double)>> m_scrollCallBackFunctions;
 		static std::vector<std::function<void(int, int, int, int)>> m_keyCallbackFunctions;
 
-
-
+		SGInputManager();
+		void RegistCallbackFunc(GLFWwindow* window);
+		static void KeyCallbackFunc(GLFWwindow* window, GLint vKey, GLint vScancode, GLint vAction, GLint vMode);
+		static void CursorCallbackFunc(GLFWwindow* window, GLdouble vPosX, GLdouble vPosY);
+		static void ScrollCallbackFunc(GLFWwindow* window, GLdouble vOffsetX, GLdouble vOffsetY);
+		static void MouseButtonCallbackFunc(GLFWwindow* window, GLint vButton, GLint vAction, GLint vModes);
 	};
 
 
